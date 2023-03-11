cad4fc667adfda3e9d1646cc37fc8a5a910137f6
Anton Stepanov <to@stepa.dev>
2023-03-11 14:25:10 +0600

    Refactoring: data is separated from the quotes table component

--- src/components/TableQuotes.tsx
+++ src/components/TableQuotes.tsx
@@ -1,39 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
-import TableQuotesItem from './TableQuotesItem';
-import { DummyData } from '../store/DummyData';
+import React from 'react';
+import { FlatList, StyleSheet, View, Text } from 'react-native';
 
-
-const data = Object.entries(DummyData).map(([key, value]) => {
-  return { tickerName: key, ...value };
-});
-
-const TableQuotes = () => {
-  const [tableData, setTableData] = useState([]);
-
-  useEffect(() => {
-    setTableData(data);
-  }, []);
-
-  const renderItem = ({ item }) => (
-    <TableQuotesItem
-      tickerName={item.tickerName}
-      last={item.last}
-      highestBid={item.highestBid}
-      percentChange={item.percentChange}
-    />
+const TableItem = ({ tickerName, last, highestBid, percentChange }) => {
+  return (
+    <View style={styles.row}>
+      <Text style={styles.cell}>{tickerName}</Text>
+      <Text style={styles.cell}>{last}</Text>
+      <Text style={styles.cell}>{highestBid}</Text>
+      <Text style={styles.cell}>{percentChange}</Text>
+    </View>
   );
+};
 
+const TableHeader = () => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerCell}>Ticker Name</Text>
+      <Text style={styles.headerCell}>Last</Text>
+      <Text style={styles.headerCell}>Highest Bid</Text>
+      <Text style={styles.headerCell}>% Change</Text>
+    </View>
+  );
+};
+
+const TableQuotes = ({ data }) => {
   return (
     <View style={styles.container}>
+      <TableHeader />
       <FlatList
-        data={tableData}
-        renderItem={renderItem}
+        data={data}
+        renderItem={({item}) => (
+          <TableItem
+            tickerName={item.tickerName}
+            last={item.last}
+            highestBid={item.highestBid}
+            percentChange={item.percentChange}
+          />
+        )}
         keyExtractor={(item) => item.tickerName}
         // Render 10 items at a time
         initialNumToRender={10}
         // Render 10 items ahead of the current window
         windowSize={10}
+        showsVerticalScrollIndicator={false}
       />
     </View>
   );
@@ -42,7 +51,32 @@ const TableQuotes = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  }
+    padding: 10,
+    backgroundColor: '#fff',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  },
+  headerCell: {
+    fontWeight: 'bold',
+    width: '25%',
+    textAlign: 'center',
+    fontSize: 14,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
+  },
+  cell: {
+    width: '25%',
+    textAlign: 'center',
+    fontSize: 11,
+  },
 });
 
 export default TableQuotes;
--- src/screens/QuotesScreen.tsx
+++ src/screens/QuotesScreen.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TableQuotes from '../components/TableQuotes';
+import { DummyData } from '../store/DummyData';
+
+const data = Object.entries(DummyData).map(([key, value]) => {
+  return { tickerName: key, ...value };
+});
 
 export const QuotesScreen = () => {
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    setTableData(data);
+  }, []);
+
   return (
-    <TableQuotes />
+    <TableQuotes data={tableData} />
   );
 }
