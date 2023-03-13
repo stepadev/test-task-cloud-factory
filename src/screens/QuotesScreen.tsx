8017036c614edf5ab9657ab14e6deaf937433af4
Anton Stepanov <to@stepa.dev>
2023-03-13 22:12:59 +0600

    Added fetching data from API

--- src/screens/QuotesScreen.tsx
+++ src/screens/QuotesScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import TableQuotes from '../components/TableQuotes';
-import { DummyData } from '../store/DummyData';
+import { useIsFocused } from '@react-navigation/native';
 
 interface IData {
   tickerName: string;
@@ -9,17 +9,43 @@ interface IData {
   percentChange: string;
 };
 
-const data: IData[] = Object.entries(DummyData)
-  .map(([key, value]) => { return { tickerName: key, ...value };
-});
-//console.log(data);
+const API_URL = 'https://poloniex.com/public?command=returnTicker';
 
 export const QuotesScreen = () => {
+  
+  const isFocused = useIsFocused(); 
   const [tableData, setTableData] = useState<IData[]>([]);
 
+  const fetchData = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      const preData = Object.keys(data).map((key) => ({
+        tickerName: key,
+        ...data[key],
+      }));
+      setTableData(preData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
   useEffect(() => {
-    setTableData(data);
-  }, []);
 
+    if (isFocused) {
+      fetchData();
+
+      const intervalId = setInterval(() => {
+        fetchData();
+      }, 5000);
+
+      return () => clearInterval(intervalId);
+    }
+
+    return () => setTableData([]);
+
+  }, [isFocused]);
+  
   return <TableQuotes data={tableData} />;
+
 };
